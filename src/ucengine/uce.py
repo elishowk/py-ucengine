650319cdf5ec25bb6ea8058dd5b0a2a71743da5e
elishowk <elishowk@nonutc.fr>
2011-08-18 12:51:49 +0200

    array urlencoded

--- src/ucengine/uce.py
+++ src/ucengine/uce.py
@@ -1,9 +1,42 @@
 import httplib
 import urllib
 import json
+from datetime import datetime
 
 from session import Session
 from core import UCError
+
+def convert(data):
+    #if isinstance(data, unicode):
+    #    return str(data)
+    if isinstance(data, datetime):
+        return data.isoformat()
+    if isinstance(data, bool):
+        return json.dumps(data)
+    elif isinstance(data, dict):
+        return dict(map(convert, data.iteritems()))
+    elif isinstance(data, (list, tuple, set, frozenset)):
+        return type(data)(map(convert, data))
+    else:
+        return data
+
+def recursive_urlencode(d):
+    def recursion(d, base=None):
+        pairs = []
+
+        for key, value in d.items():
+            if hasattr(value, 'values'):
+                pairs += recursion(value, key)
+            else:
+                new_pair = None
+                if base:
+                    new_pair = "%s[%s]=%s" % (base, urllib.quote(unicode(key)), urllib.quote(unicode(value)))
+                else:
+                    new_pair = "%s=%s" % (urllib.quote(unicode(key)), urllib.quote(unicode(value)))
+                pairs.append(new_pair)
+        return pairs
+
+    return '&'.join(recursion(d))
 
 class UCEngine(object):
     "The Server"
@@ -16,19 +49,23 @@ class UCEngine(object):
     def request(self, method, path, body=None):
         "ask something to the server"
         connection = httplib.HTTPConnection(self.host, self.port)
+
         if body != None:
+            encodedbody = convert(body)
             connection.request(method, '/api/0.6%s' % path,
-                urllib.urlencode(body))
+                recursive_urlencode(encodedbody))
+            # print
+            print recursive_urlencode(encodedbody)
         else:
             connection.request(method, '/api/0.6%s' % path)
         resp = connection.getresponse()
         raw = resp.read()
         try:
             response = json.loads(raw)
-        except ValueError as e:
-            #print raw
+        except ValueError:
             response = None
         connection.close()
+        print resp.status, response
         return resp.status, response
 
     def connect(self, user, credential):
