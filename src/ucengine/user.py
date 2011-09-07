a8484723df808588ed7a7b5ca98dc2a1d3a07c25
elishowk <elishowk@nonutc.fr>
2011-09-07 11:29:15 +0200

    auth type param

--- src/ucengine/user.py
+++ src/ucengine/user.py
@@ -4,20 +4,22 @@ class Client(object):
     "A simple client"
 
 
-    def __init__(self, name, credential=None, uid=None, auth=None, metadata=None):
+    def __init__(self, name, uid=None, credential=None, auth=None, metadata=None):
         self.name = name
         self.metadata = metadata
         self.credential = credential
         self.uid = uid
-        self.auth = auth or 'password'
+        self.auth = auth
 
 
 class User(Client):
     "A user"
-    def __init__(self, name, uid=None, credential=None, metadata={}):
-        if metadata.has_key('nickname'):
+    def __init__(self, name, uid=None, credential=None, auth=None, metadata=None):
+        if metadata is None:
+            metadata = {}
+        elif metadata.has_key('nickname'):
             metadata['nickname'] = name
-        Client.__init__(self, name, uid, credential, metadata)
+        Client.__init__(self, name, uid, credential, auth, metadata)
 
     def __repr__(self):
         return "<User name:%s>" % self.name
