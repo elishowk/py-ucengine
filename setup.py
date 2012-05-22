61eb39c4368d80d45a2b81c651207a4f6418ab7e
elishowk <elishowk@nonutc.fr>
2012-05-22 17:08:53 +0200

    updated deps

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='py-ucengine',
       version='0.6',
